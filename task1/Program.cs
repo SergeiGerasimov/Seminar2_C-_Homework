e398e1625873707dc9eefee0abeda08c8b563fc6
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-08 11:45:39 +0700

    SecondDigit

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число -> {number}");
+
+int SecondDigit(int num)
+{
+    while (number >= 100) number = number - 100;
+    int firstDigit = number / 10;
+    return firstDigit;
+}
+
+int result = SecondDigit(number);
+Console.WriteLine($"Второе число - {result}");
+
