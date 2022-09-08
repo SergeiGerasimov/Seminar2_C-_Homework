8b88a3bca69e11a2aa1503bb61a14c75dc2e82ab
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-08 20:39:53 +0700

    Checkingdayweeking

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,3 +3,18 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Ура, уже выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это вообще не день недели");
+  }
+  else Console.WriteLine("Увы, еще не выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
