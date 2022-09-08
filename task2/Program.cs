6606eee6593434deab13ecd81706193a9465e830
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-08 20:23:34 +0700

    Third Number

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+int number = 645;//new Random().Next(100, 23000);
+Console.WriteLine($"Случайное трехзначное число -> {number}");
+
+
+while (number >= 1000) number = number / 10;
+if (number >= 100)
+{
+    number = number % 10;
+    Console.WriteLine($"Третье число - {number}");
+}
+else
+{
+    Console.WriteLine($"Третьего числа нет ");
+}
